36a61a0a34a7db2163862ce3a95a1bc09badb224
deveshahuja <deveshahuja9@gmail.com>
2024-08-29 02:29:40 +0530

    Day 0

---
+++ code_289B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char findTheDifference(string s, string t)
+{
+    char c=(char)0;
+    int i, size = s.size();
+    for (i = 0; i < size; ++i) {
+        c^=s[i];
+        c^=t[i]; 
+    }
+    c^=t[i];
+    return c;
+}
+
+int main()
+{
+    string s = "abc", t = "bcae";
+    char ans = findTheDifference(s, t);
+    cout << ans;
+ 
+    return 0;
+}

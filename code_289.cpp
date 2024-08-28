5207b0d12c7cde0bd5913d9072d5a55d6752a550
deveshahuja <deveshahuja9@gmail.com>
2024-08-29 02:21:43 +0530

    Day 0

---
+++ code_289.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char findTheDifference(string s, string t)
+{
+    map<char, int> tmap;
+    char ans;
+    for(int i = 0; i<t.size(); i++){
+        if(tmap.count(t[i])) tmap[t[i]]++;
+        else tmap[t[i]] = 1;
+    }
+
+    for(int i = 0; i<s.size(); i++){
+        if(tmap.count(s[i])) tmap[s[i]]--;
+    }
+
+    map<char, int>::iterator it = tmap.begin();
+    while (it != tmap.end()) {        
+        if(it->second) ans = it->first;
+        ++it;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string s = "a", t = "aa";
+    char ans = findTheDifference(s, t);
+    cout << ans;
+ 
+    return 0;
+}

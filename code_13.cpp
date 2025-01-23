a77651c5532a15de5aa6e5483bd254a4aa9e6b33
deveshahuja <deveshahuja9@gmail.com>
2025-01-24 01:05:47 +0530

    Day 3:2

---
+++ code_13.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int romanToInt(string s) {
+    unordered_map<char, int> roman = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    int sum = 0;
+    for (int i = 0; i + 1 < s.length(); ++i) {
+        if (roman[s[i]] < roman[s[i + 1]])
+            sum -= roman[s[i]];
+        else
+            sum += roman[s[i]];
+    }
+    return sum + roman[s.back()];
+}
+
+int main()
+{
+    string s = "MCMXCIV";
+    cout << romanToInt(s);
+    return 0;
+}
